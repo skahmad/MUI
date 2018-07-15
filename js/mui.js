86e0dad54c347c4d37d9be0694d60cda9f6f1414
Sk Ahmad <ahmad.sk98@gmail.com>
2018-07-15 09:38:34 +0530

    window icons are added. mui.js testing.

--- js/mui.js
+++ js/mui.js
@@ -1,1 +1,9 @@
-console.log('Wel come to use my css framework');
+// Moon User Interface Controller program code in Java Script
+// it uses Jquery Ui version 3.3.1
+var themes = ["windows","firefox","modern"];
+
+$('document').ready(function() {
+    console.log("Loaded");
+    var m = $('meta');
+    console.log(m);
+});
